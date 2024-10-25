05b8dd6119a67cae7e41282f9f96a4e5c4cbb6ff
hubert <hubert.szelepusta@gmail.com>
2024-10-25 23:05:36 +0200

    lab3 working with files

---
+++ 99707_lab3/funkcje.php
@@ -0,0 +1,67 @@
+<?php
+
+if (isset($_REQUEST["submit"])) {
+    $akcja = $_REQUEST["submit"]; 
+    switch ($akcja) {
+    case "zapisz":dodaj();break;
+    case "pokaz":pokaz_zamowienie("");break;
+    case "php":pokaz_zamowienie("PHP");break;
+    case "cpp":pokaz_zamowienie("CPP");break;
+    case "java":pokaz_zamowienie("Java");break;
+    }
+}
+
+function dodaj() {
+    $dane = "";
+    if (isset($_REQUEST["nazwisko"])&&isset($_REQUEST["wiek"])&&isset($_REQUEST["email"])&&isset($_REQUEST["panstwo"])&&isset($_REQUEST["sposobyPlatnosci"])) {
+        $dane .= htmlspecialchars($_REQUEST['nazwisko']).", ";
+        $dane .= htmlspecialchars($_REQUEST['wiek']).", ";
+        $dane .= htmlspecialchars($_REQUEST['email']).", ";
+        $dane .= htmlspecialchars($_REQUEST['panstwo']).", ";
+        $dane .= htmlspecialchars($_REQUEST['sposobyPlatnosci']).", ";
+    }
+    else{
+        echo "Nie podano wszystkich danych";
+        return;
+    }
+    $wybraneTutoriale="";
+    foreach($_REQUEST['jezyki'] as $jezyk) {
+        $wybraneTutoriale .= $jezyk.", ";
+    }
+    $dane .=substr($wybraneTutoriale,0,strlen($wybraneTutoriale)-2)."\n";
+    @ $wp = fopen("dane.txt","a",1);
+    if ($wp)
+    { 
+        fwrite($wp,$dane);
+        exit;
+    }
+    fclose($wp);
+}
+
+function pokaz_zamowienie($tut) {
+    @ $plik = file("dane.txt");
+    if ($plik)
+    { 
+        echo "
+        <table border=1>
+        <tr>
+            <th>Nazwisko</th><th>Wiek</th><th>Email</th><th>Panstwo</th><th>Sposob platnosci</th><th colspan=9>Jezyki</th>
+        </tr>";
+        for ( $i=0; $i<count($plik); $i++)
+        {
+            $dane = explode(",",$plik[$i]);
+            if(str_contains(implode($dane), $tut)){
+                echo "<tr>";
+                for( $j=0;$j<count($dane);$j++){
+                        echo "<td>".rtrim($dane[$j], ',')."</td>";
+                }
+                echo "</tr>";
+            }
+        }
+        echo "</table>";
+        exit;
+    }
+
+    fclose($plik);
+   }   
+?>
